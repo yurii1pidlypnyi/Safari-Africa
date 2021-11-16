ea4bb90967249eac6e93c94aa37c06bf4717dca5
Yurii <pidlypniyuyra@gmai.com>
2021-11-16 13:08:54 +0200

    добавлено слайдер в секцію країни

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Burger menu
 let burger = document.querySelector('.menu__burger');
 let menu = document.querySelector('.header__menu');
 let headerTop = document.querySelector('.header__top')
@@ -8,10 +9,8 @@ burger.addEventListener('click',()=>{
   headerTop.classList.toggle('active')
   body.classList.toggle('lock');
 })
-
-
-//Слайдер
-
+//--------------------------------------------------------
+//Checkbox style
 let check = document.querySelector('.agreement');
 let agree = document.querySelector('.agree');
 
@@ -22,4 +21,39 @@ agree.addEventListener('click', ()=>{
     check.checked = true
   }
   agree.classList.toggle('active');
+});
+//--------------------------------------------------------
+//Slider
+let width = window.innerWidth;
+slider(width);
+
+window.addEventListener('resize',()=>{
+  width = window.innerWidth;
+  slider(width)
 })
+
+function slider (size){
+  if (size < 769){
+    $('.slider').not('.slick-initialized').slick({
+      arrows: true,
+      dots: true,
+      slidesToShow: 1,
+      infinite: false,
+      waitForAnimate: false,
+      mobileFirst: true,
+      responsive: [
+      {
+        breakpoint: 769,
+        settings: "unslick"
+      },
+      {
+        breakpoint: 410,
+        settings: {
+          slidesToShow:2
+        }
+      },
+    ]
+    })
+  }
+}
+//-----------------------
